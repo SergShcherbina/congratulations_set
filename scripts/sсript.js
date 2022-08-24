8fb50b70b0c1cc6749969e0061dc546a78d906b0
Serg <1990sergik27@gmail.com>
2022-08-24 16:09:37 +0300

    add_js_code

--- scripts/sсript.js
+++ scripts/sсript.js
@@ -1,35 +1,77 @@
-const buttonMan = document.querySelector('.header__button-gender_men');
-const buttonWoman = document.querySelector('.header__button-gender_women');
+const buttonMen = document.querySelector('.header__button-gender_men');
+const buttonWomen = document.querySelector('.header__button-gender_women');
 const body = document.body;
+const cardText = document.querySelector('.card__text');
+const cardImage = document.querySelector('.card__img');
+const btnChangeText = document.querySelector('.header__button-change_text');
+const btnChangeImg = document.querySelector('.header__button-change_img');
 
-const chengeToMan = () => {
-    if(!body.classList.contains('man')){
-        body.classList.add('man');
-        body.classList.remove('woman');
-    }
-    
-};
-const chengeToWoman = () => {
-    if(!body.classList.contains('woman')){
-        body.classList.add('woman');
-        body.classList.remove('man');
-    }
-};
-
-buttonMan.addEventListener('click', chengeToMan);
-
-buttonWoman.addEventListener('click', chengeToWoman);
-
-
-//второй вариант
-/* const chengeToGender = (gender) => {
-    body.className = gender;
+const state = {                                                            //с помощью обьекта отслеживаем класс в body 
+    gender: body.classList.contains('women') ? 'women' : 'men'                   
 }
 
-buttonMan.addEventListener('click', () => {
-    chengeToGender('man')
-});
+const getRandomForArr = (arr) => {                                         //рандомные числа подставляем в индекс масива
+    let randomNumber = Math.floor(Math.random() * arr.length);
+    return arr[randomNumber];
+};
 
-buttonWoman.addEventListener('click', () => {
-    chengeToGender('woman')
-}); */
+const getData = () => {                                                    //получаем промис с базы данных
+    return fetch('db.json').then(response => response.json());
+};
+
+const changeDOM = () => {
+    if(state.photo.includes('black')){                                     //если в названии картинки есть слово black то
+        cardText.style = 'color: #fff';
+    } else {
+        cardText.style = '';
+    }
+
+    cardImage.src = `img/${state.photo}`;                                  //в путь картинки прописываем через интерполяцию название картинки 
+    cardText.innerHTML = state.text.replaceAll('/n', '<br/>');             //в полученом тексте все переносы /n меняем на <br/>   
+};
+
+const getDateToCard = () => {                                              //парсим промис и вызываемв рандом с аргументом из данных промиса
+    getData().then(data => {
+        state.text = getRandomForArr(data.text[state.gender]);             //результат звписываем в обьект state
+        state.photo = getRandomForArr(data.photo[state.gender]);
+        changeDOM();
+    });
+}
+
+getDateToCard();
+const changeToMen = () => {
+    if(state.gender !== 'men'){
+        body.classList.add('men');
+        body.classList.remove('women');
+        state.gender = 'men'
+        getDateToCard();
+    }
+};
+
+const chengeToWomen = () => {
+    if(state.gender !== 'women'){
+        body.classList.add('women');
+        body.classList.remove('men');
+        state.gender = 'women'
+        getDateToCard();
+    }
+};
+
+const changeText = () =>{
+    getData().then(data => {
+        state.text = getRandomForArr(data.text[state.gender]);
+        changeDOM();
+    });
+};
+
+const changeImg = () =>{
+    getData().then(data => {
+        state.photo = getRandomForArr(data.photo[state.gender]);
+            changeDOM();
+    });
+};
+
+buttonMen.addEventListener('click', changeToMen);
+buttonWomen.addEventListener('click', chengeToWomen);
+btnChangeText.addEventListener('click', changeText);
+btnChangeImg.addEventListener('click', changeImg);
